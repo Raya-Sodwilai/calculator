ab160e08af2d0ae2ca983b0429a90d9b98f59724
Raya Sodwilai <sodwilai@pdx.edu>
2021-02-11 11:35:31 -0800

    Add subtraction, multiplication, and division to the calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,30 @@ $(document).ready(function() {
 		const number1 = parseInt($("#add1").val());
 		const number2 = parseInt($("#add2").val());
 		const result = add(number1, number2);
-		$("#output").text(result);
+		$("#add-output").text(result);
+	});
+
+	$("form#subtract").submit(function(event) {
+		event.preventDefault();
+		const number3 = parseInt($("#subtract3").val());
+		const number4 = parseInt($("#subtract4").val());
+		const result = subtract(number3, number4);
+		$("#sub-output").text(result);
+	});
+
+	$("form#multiply").submit(function(event) {
+		event.preventDefault();
+		const number5 = parseInt($("#multiply5").val());
+		const number6 = parseInt($("#multiply6").val());
+		const result = multiply(number5, number6);
+		$("#multi-output").text(result);	
+	});
+
+	$("form#divide").submit(function(event) {
+		event.preventDefault();
+		const number7 = parseInt($("#divide7").val());
+		const number8 = parseInt($("#divide8").val());
+		const result = divide(number7, number8);
+		$("#div-output").text(result);	
 	});
 });
